4824796e769a9949bc121984e71dfe2d69f24768
Michael Hudson <mnkotagu@gmail.com>
2023-09-28 11:13:24 +0300

    Implement 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+
+"""
+This module contains pascal's triangle function
+"""
+def pascal_triangle(n):
+    """
+       Generate a pascal's triangle with specified
+       number of rows
+
+       Args:
+            n: The number of rows of the Pascal's triangle
+    """
+    triangle = []
+    if n >= 1:
+        triangle.append([1])
+    
+    if n >= 2:
+        triangle.append([1, 1])
+    
+    if n >= 3:
+        for i in range(3, n + 1):
+            previous_row = triangle[i - 2]
+            new_row = [1]
+            
+            for j in range(0, len(previous_row)):
+                try:
+                    new_row.append(previous_row[j] + previous_row[j + 1])
+                except IndexError:
+                    new_row.append(1)
+            
+            triangle.append(new_row)
+                                    
+    return triangle
+        
