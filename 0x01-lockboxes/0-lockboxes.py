51fe8d237774b8c18194c2ec9d91e9226e7f4603
Michael Hudson <mnkotagu@gmail.com>
2023-10-05 17:10:59 +0300

    Attempt alternative solution to 0-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,29 +12,15 @@ def canUnlockAll(boxes):
             boxes ((List[List[int]])): A list of lists of boxes
        Return: True if all boxes can be opened ELSE, return False
     """
-    visited_indexes = set()
-    iterate_over_boxes(boxes, 0, visited_indexes)
-    return len(visited_indexes) == len(boxes)
+    visited_indexes, indexes_to_visit = set(), {0}
+    total_boxes = len(boxes)
 
+    while indexes_to_visit:
+        current_index = indexes_to_visit.pop()
+        visited_indexes.add(current_index)
 
-def iterate_over_boxes(boxes, index, visited_indexes):
-    """
-        Recursively iterates over the boxes from a given index while keeping
-        track of the visited indexes
+        for index in boxes[current_index]:
+            if index < total_boxes and index not in visited_indexes:
+                indexes_to_visit.add(index)
 
-        Args:
-            boxes (List[List[int]]): A list of lists of boxes
-            index (int): The index to currently stat the box iteration.
-            visited_indexes (set): A set visited box indexes.
-
-        Returns:
-            visited_indexes (set): A set containing all visited indexes
-    """
-    if index in visited_indexes:
-        return
-
-    visited_indexes.add(index)
-
-    for key in boxes[index]:
-        if key < len(boxes):
-            iterate_over_boxes(boxes, key, visited_indexes)
+    return len(visited_indexes) == total_boxes
