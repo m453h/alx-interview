68933a765686360db2350dba5ede373ccea5f216
Michael Hudson <mnkotagu@gmail.com>
2023-10-26 20:31:51 +0300

    Implement solution for UTF-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This module contains the function that solves the UTF-8 validation challenge
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding
+
+    Args:
+        data (list): A list of integers representing the UTF-8
+                    set of characters
+
+    Return:
+            True if is a valid UTF-8 encoding ELSE, return False
+    """
+    i = 0
+    while i < len(data):
+        binary_block = format(data[i], '08b')
+        if binary_block.startswith('0'):
+            i += 1
+        elif binary_block.startswith('110'):
+            if i + 1 < len(data) \
+                    and format(data[i + 1], '08b').startswith('10'):
+                i += 2
+            else:
+                return False
+        elif binary_block.startswith('1110'):
+            if i + 2 < len(data) \
+                    and format(data[i + 1], '08b').startswith('10') \
+                    and format(data[i + 2], '08b').startswith('10'):
+                i += 3
+            else:
+                return False
+        elif binary_block.startswith('11110'):
+            if i + 3 < len(data) \
+                    and format(data[i + 1], '08b').startswith('10') \
+                    and format(data[i + 2], '08b').startswith('10') \
+                    and format(data[i + 3], '08b').startswith('10'):
+                i += 4
+            else:
+                return False
+        else:
+            return False
+
+    return True
