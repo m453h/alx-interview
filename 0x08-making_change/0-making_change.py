7690c7e36fcf094b4c64959687725f4d7c83be46
Michael Hudson <mnkotagu@gmail.com>
2023-11-23 14:56:24 +0300

    Improve project README.md and implement solution prototype

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains the function that solves the making change problem
+"""
+
+
+def makeChange(coins, total):
+    """
+       Determines the fewest number of coins needed to meet a given
+       amount total
+
+       Args:
+            coins (List[int]): A list of the values of the coins
+            total: The total amount needing change
+       Return: (int) The total number of coins that need to be returned
+                -1 if total cannot be met by any number of coins present
+
+    """
+    pass
+
